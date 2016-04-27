e1fde1cffc4e787a2583f57cfc0e840316ded6cb
Himanshu Pal <pal.himanshu1991@gmail.com>
2016-04-27 17:49:56 +0530

    Commit from phpstorm

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -2,17 +2,21 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Http\Requests;
-use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreBookRequest;
+use App\Http\Requests\UpdateBookRequest;
 use App\Models\Book;
+use App\Repositories\AuthorRepository;
 use App\Repositories\BookRepository;
+use App\Repositories\ReviewRepository;
+use Illuminate\Http\Request;
 
-class BookController extends Controller {
+class BookController extends Controller
+{
 
     /**
      * The book repository instance
-     * 
+     *
      * @var BookRepository
      */
     protected $books;
@@ -20,9 +24,10 @@ class BookController extends Controller {
     /**
      * Create a new controller instance.
      *
-     * @return void
+     * @param BookRepository $books
      */
-    public function __construct(BookRepository $books) {
+    public function __construct(BookRepository $books)
+    {
         $this->middleware('auth');
         $this->books = $books;
     }
@@ -32,8 +37,8 @@ class BookController extends Controller {
      *
      * @return \Illuminate\Http\Response
      */
-    public function index() {
-
+    public function index()
+    {
         $books = $this->books->getAll();
         return view('books.index', compact('books'));
     }
@@ -43,29 +48,32 @@ class BookController extends Controller {
      *
      * @return \Illuminate\Http\Response
      */
-    public function create() {
-        //$this->authorize('create');
+    public function create()
+    {
         return view('books.create');
     }
 
     /**
      * Create a new book.
      *
-     * @param  Request  $request
+     * @param  Request $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Requests\StoreBookRequest $request) {        
+    public function store(StoreBookRequest $request)
+    {
         $this->books->create($request->all());
         return redirect('/books');
     }
 
+
     /**
      * Display a book
-     * 
-     * @param type $id
-     * @return type
+     *
+     * @param Book $book
+     * @return mixed
      */
-    public function show(Book $book) {       
+    public function show(Book $book)
+    {
         $book = $this->books->find($book->id);
         return view('books.show', compact('book'));
     }
@@ -73,10 +81,11 @@ class BookController extends Controller {
     /**
      * Display a form to edit a book.
      *
-     * @param int $id
-     * @return \Illuminate\Http\Response
+     * @param Book $book
+     * @return \Illuminate\Http\Response     
      */
-    public function edit(Book $book) {
+    public function edit(Book $book)
+    {
         $this->authorize('edit', $book);
         $book = $this->books->find($book->id);
         return view('books.edit', compact('book'));
@@ -85,22 +94,23 @@ class BookController extends Controller {
     /**
      * Update a book.
      *
-     * @param  Request  $request
+     * @param  Request $request
      * @return \Illuminate\Http\Response
      */
-    public function update(Requests\UpdateBookRequest $request, Book $book) {
-
+    public function update(UpdateBookRequest $request, Book $book)
+    {
         $this->books->update($request->all(), $book->id);
         return redirect('books');
     }
 
     /**
      * Delete a book
-     * 
+     *
      * @param type $id
      * @return \Illuminate\Http\Response
      */
-    public function destory($id) {
+    public function destory($id)
+    {
 
         $this->books->delete($id);
         return redirect('/books');
